5690b420e036fb4d3a3ace834a7151d81331f5a2
Shahzade Alam <shahzade.alam707@gmail.com>
2025-10-14 13:23:24 +0530

    google analytic

--- resources/views/home/includes/head.blade.php
+++ resources/views/home/includes/head.blade.php
@@ -11,8 +11,6 @@
 
 <link rel="stylesheet" href="{{ asset('home/Scripts/Calendar/theme1.css') }}" type="text/css" />
 <link href="{{ asset('home/site/writereaddata/config/pagescssstyles/en-home-template.css') }}" rel="stylesheet" />
-
-<!-- Include all below css library for Dynamic Work and should not be removed from here. -->
 <link href="{{ asset('home/css/bootstrap.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('home/css/font-awesome.min.css') }}" rel="stylesheet" />
 <link rel="stylesheet" type="text/css" href="{{ asset('home/css/jquery.smartmenus.bootstrap.css') }}" />
@@ -29,16 +27,29 @@
 <link rel="stylesheet" href="{{ asset('home/css/jquery.flipster.min.css') }}" />
 <link href="{{ asset('home/css/responsive.css') }}" rel="stylesheet" />
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S2E3VENDPG"></script>
+<!-- Google Analytics (GA4) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PRDT0Y2NKY"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-S2E3VENDPG');
+  // Default consent mode blocked for EU/EEA users
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied'
+  });
+
+  // Config GA
+  gtag('config', 'G-PRDT0Y2NKY');
 </script>
+
+<!-- Google AdSense -->
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2658553012188142"
-crossorigin="anonymous">
-</script>
+crossorigin="anonymous"></script>
 <meta name="google-adsense-account" content="ca-pub-2658553012188142">
+
+<!-- Consent Manager for EU/EEA users -->
+<script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/e5ed9588022f4.js"
+        data-cmp-host="a.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
+
