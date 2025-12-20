4005e396c725d538730c8d4bd85c11c61dcc1fb3
Shahzade Alam <shahzade.alam707@gmail.com>
2025-12-20 14:05:50 +0530

    khwaja sahab

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -22,7 +22,7 @@
                      class="img-fluid" style="height:300px; object-fit:cover;">
             </div>
             <div class="col-md-3 col-sm-6">
-                <img src="{{ asset('home/images/dewasharif/event/event-2.jpeg') }}" 
+                <img src="{{ asset('home/images/dewasharif/event/event-five (1).jpeg') }}" 
                      class="img-fluid" style="height:300px; object-fit:cover;">
             </div>
             <div class="col-md-3 col-sm-6">
@@ -30,7 +30,7 @@
                      class="img-fluid" style="height:300px; object-fit:cover;">
             </div>
             <div class="col-md-3 col-sm-6">
-                <img src="{{ asset('home/images/dewasharif/event/event-7.jpeg') }}" 
+                <img src="{{ asset('home/images/dewasharif/event/event-five (2).jpeg') }}" 
                      class="img-fluid" style="height:300px; object-fit:cover;">
             </div>
         </div>
