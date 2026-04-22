4bf8a9a4767461a906b190b9b8ad3c1632737b53
Shahzade Alam <shahzade.alam707@gmail.com>
2026-04-22 17:31:10 +0530

    masjid

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -113,7 +113,7 @@
             </div>
             <div class="col-md-2 leader ">
                 <ul>
-                    <li><img src="home/images/dewasharif/shahzade.jpeg" alt="Shahzade Alam"
+                    <li><img src="home/images/dewasharif/IUL.jpeg" alt="Shahzade Alam"
                             title="Shahzade Alam" />
                         <h4>Shahzade Alam <br>[مشرت وارثی]</h4>
                         <p><b><i>Software Developer</i></b> <br /> <b>www.dewasharif.com</b></p>
