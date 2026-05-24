a8802768fe704bc96b90b1e446188c1f7a2c0a00
Shahzade Alam <shahzade.alam707@gmail.com>
2026-05-24 13:05:13 +0530

    advertise

--- resources/views/home/islam/thoughts/goodthoughts.blade.php
+++ resources/views/home/islam/thoughts/goodthoughts.blade.php
@@ -1,29 +1,148 @@
 @extends('home.includes.master')
 @section('content')
-<div class="container">
-    <div class="maroon">
-        <h1>*** Good Thoughts ***</h1><br>
-         <h1>We Love Allah (s.w.t)</h1> <br>
-         <h1>We Love Muhammad (s.a.w)</h1>
-    </div>
-    <div class="row">
-        <div class="col-sm-12">
-            <p>
+    <div class="container">
+        <div style="text-align:center; padding:20px 10px;">
 
+            <h1 style="margin-bottom:18px; font-size:35px;">*** Good Thoughts ***</h1>
 
-            </p>
-           
+            <h1 style="margin-bottom:18px; font-size:26px;">We Love Allah (s.w.t)</h1>
+
+            <h1 style="margin-bottom:18px; font-size:26px;">We Love Muhammad (s.a.w)</h1>
+
         </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <p>
 
- 
+
+        <div style="display:flex; flex-wrap:wrap; margin-top:25px;">
+
+            <!-- LEFT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Ash-Sharh (94:6)<br><br>
+
+                    اِنَّ مَعَ الْعُسْرِ يُسْرًا<br>
+                    “Beshak mushkil ke saath aasani hai.”<br>
+                    “Indeed, with hardship comes ease.”
                 </p>
-
             </div>
-        </div>
-    </div>
 
-</div>
+            <!-- RIGHT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Ra'd (13:28)<br><br>
+
+                    أَلَا بِذِكْرِ اللَّهِ تَطْمَئِنُّ الْقُلُوبُ<br>
+                    “Allah ki yaad hi dilon ko sukoon deti hai.”<br>
+                    “Surely, in the remembrance of Allah do hearts find peace.”
+                </p>
+            </div>
+
+        </div>
+
+        <div style="display:flex; flex-wrap:wrap; margin-top:25px;">
+
+            <!-- LEFT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah At-Talaq (65:3)
+
+                    وَمَن يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ<br>
+                    “Jo Allah par bharosa kare, Allah uske liye kaafi ho jata hai.”<br>
+                    “Whoever trusts Allah — He is sufficient for him.”
+                </p>
+            </div>
+
+            <!-- RIGHT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Al-Baqarah (2:286)
+
+                    لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا<br>
+                    “Allah kisi jaan par uski taqat se zyada bojh nahi daalta.”<br>
+                    “Allah does not burden a soul beyond what it can bear.”
+                </p>
+            </div>
+
+        </div>
+
+        <div style="display:flex; flex-wrap:wrap; margin-top:25px;">
+
+            <!-- LEFT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Al-Imran (3:139)
+
+                    وَلَا تَهِنُوا وَلَا تَحْزَنُوا<br>
+                    “Na kamzor padho, na gham karo.”<br>
+                    “Do not lose hope, and do not be sad.”
+                </p>
+            </div>
+
+            <!-- RIGHT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Az-Zumar (39:53)
+
+                    لَا تَقْنَطُوا مِن رَّحْمَةِ اللَّهِ<br>
+                    “Allah ki rehmat se kabhi mayoos mat ho.”<br>
+                    “Do not despair of the mercy of Allah.”
+                </p>
+            </div>
+
+        </div>
+
+        <div style="display:flex; flex-wrap:wrap; margin-top:25px;">
+
+            <!-- LEFT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Al-Inshirah (94:5)
+
+                    فَإِنَّ مَعَ الْعُسْرِ يُسْرًا<br>
+                    “Har takleef ke saath ek aasani hoti hai.”<br>
+                    “For indeed, with hardship comes ease.”
+                </p>
+            </div>
+
+            <!-- RIGHT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Al-Anfal (8:46)
+
+                    وَاصْبِرُوا ۚ إِنَّ اللَّهَ مَعَ الصَّابِرِينَ<br>
+                    “Sabr karo, Allah sabr karne walon ke saath hai.”<br>
+                    “Be patient; surely Allah is with the patient.”
+                </p>
+            </div>
+
+        </div>
+
+        <div style="display:flex; flex-wrap:wrap; margin-top:25px;">
+
+            <!-- LEFT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah Al-Baqarah (2:152)
+
+                    فَاذْكُرُونِي أَذْكُرْكُمْ<br>
+                    “Tum mujhe yaad karo, main tumhe yaad karunga.”<br>
+                    “Remember Me, and I will remember you.”
+                </p>
+            </div>
+
+            <!-- RIGHT COLUMN -->
+            <div style="width:50%; padding:10px; min-width:300px;">
+                <p style="font-size:18px; line-height:1.6;">
+                    ☪ Surah At-Taubah (9:51)
+
+                    قُل لَّن يُصِيبَنَا إِلَّا مَا كَتَبَ اللَّهُ لَنَا<br>
+                    “Hume wahi hota hai jo Allah ne humare liye likh diya.”<br>
+                    “Nothing will happen to us except what Allah has written for us.”
+                </p>
+            </div>
+
+        </div>
+
+
+
+    </div>
 @endsection
